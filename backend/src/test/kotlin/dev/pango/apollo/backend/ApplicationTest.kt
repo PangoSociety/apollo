fffb491f7dd7ea6e9da95bd5e9ef98f875071b2a
Frank Cary <fcaryv@MacBook-Pro.local>
2023-12-26 14:43:16 -0500

    [AP-02] fix: removing tests

--- backend/src/test/kotlin/dev/pango/apollo/backend/ApplicationTest.kt
+++ backend/src/test/kotlin/dev/pango/apollo/backend/ApplicationTest.kt
@@ -1,21 +1,16 @@
 package dev.pango.apollo.backend
 
-import io.ktor.http.HttpStatusCode
-import io.ktor.server.testing.testApplication
-import junit.framework.TestCase.assertEquals
-import kotlin.test.Test
-
 class ApplicationTest {
 //    @Test
 //    fun testRoot() =
 //        testApplication {
-////            application {
-////                configureRouting()
-////            }
+// //            application {
+// //                configureRouting()
+// //            }
 //            assertEquals(HttpStatusCode.OK, HttpStatusCode.OK)
-////        client.get("/").apply {
-////            assertEquals(HttpStatusCode.OK, status)
-////            assertEquals("Hello World!", bodyAsText())
-////        }
+// //        client.get("/").apply {
+// //            assertEquals(HttpStatusCode.OK, status)
+// //            assertEquals("Hello World!", bodyAsText())
+// //        }
 //        }
 }
